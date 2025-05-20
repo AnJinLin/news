65c3a8567b698dbe30efe4f2cd966ebe0e9d2603
AnJinLin <148747248+AnJinLin@users.noreply.github.com>
2025-05-20 16:07:21 +0800

    Add files via upload

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7c+(b@#_enrq2fn^fu-$khhtzkg-fy0m31l5uv+j=fm+_a$xrv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
